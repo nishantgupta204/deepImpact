947441c57e921fa50b276667fbf2236fbf9de61e
Jeff Groom <jgroom@ciena.com>
2016-08-25 11:15:49 -0700

    service detail

--- client/views/ciena_service_detail/ciena_service_detail.js
+++ client/views/ciena_service_detail/ciena_service_detail.js
@@ -1,72 +1,72 @@
-Template.CienaServiceDetail.rendered = function() {
-
+Template.CienaServiceDetail.rendered = function () {
+  Session.set('service', {});
 };
 
 Template.CienaServiceDetail.events({
-    'click .service-delete':function(event, Ciena) {
-        alert(JSON.stringify(this))
-        $('#serviceDeleteConfirmModal').modal('show');
-    },
-    'click .endpoint-remove':function(event, Ciena) {
-    	Meteor.call("mdso_removeEndpoint", this, function (error, result) {
-    	   Router.go('ciena_services');
-      });
-    }
+  'click .service-delete': function (event, Ciena) {
+    alert(JSON.stringify(this))
+    $('#serviceDeleteConfirmModal').modal('show');
+  },
+  'click .endpoint-remove': function (event, Ciena) {
+    Meteor.call("mdso_removeEndpoint", this, function (error, result) {
+      Router.go('ciena_services');
+    });
+  }
 });
 
 Template.CienaServiceDetail.helpers({
-    'serviceID': function() {
+  'serviceID': function () {
     return Session.get("serviceID");
   },
-     'service': function() {
+  'service': function () {
     return Session.get("service");
   },
-    orchStateIs: function(orchState){
+  orchStateIs: function (orchState) {
     return this.orchState === orchState;
   },
 });
 
 Template.CienaServiceDetail.created = function () {
-    var str = Iron.Location.get().path;
-    var n = str.lastIndexOf('/');
-    var sid = str.substring(n + 1);
-    var sid = this.data.params.id;
-    Session.set('serviceID',{"id":sid});
-    var service = new Object();
-	  Meteor.call("mdso_getProductsByLabel", sid, "raciena6x.resourceTypes.XvcFragment", function (error, result) {
+  var str = Iron.Location.get().path;
+  var n = str.lastIndexOf('/');
+  var sid = str.substring(n + 1);
+  var sid = this.data.params.id;
+  Session.set('serviceID', { "id": sid });
+  var service = new Object();
+  Meteor.call("mdso_getProductsByLabel", sid, "raciena6x.resourceTypes.XvcFragment", function (error, result) {
     if (result) {
       service = result;
 
-      $.each(result, function(index, item) {
+      $.each(result, function (index, item) {
         Meteor.call("mdso_getResourceById", item.properties.device, function (error, result2) {
           if (result2) {
             // service.details.push(result2);
             service[index].details = result2;
-            service[index].hostname = result2.label;
+            Session.set('service', service);
           }
         });
       });
-    }
-    Session.set('service', service);
-    console.log("service: " + service);
+    console.log("service: ");
     console.log(service);
+    }
+
 
   });
 };
 
 Template.serviceDeleteConfirmModal.events({
-  'click .__cancel': function(e, t) {
+  'click .__cancel': function (e, t) {
     Modal.hide();
   },
 
-  'click .__delete': function(e, t) {
+  'click .__delete': function (e, t) {
     var self = this;
     Meteor.call('cart.removeLineItem', {
       cartId: self.product.parent._id,
       lineItemId: self.product._id,
-    }, function(err){
+    }, function (err) {
       Modal.hide();
-      if(!err){
+      if (!err) {
         toastr.success('Item removed.', 'Success');
         analytics.track('Deleted a cart line item');
       } else {
